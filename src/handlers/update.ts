44cbef5244708ce89ab57722a2007a511829e421
jgaribsin <16392336+jgaribsin@users.noreply.github.com>
2024-03-20 21:12:34 -0600

    #none using expiermental message "caching" for message updates

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -1,4 +1,4 @@
-import {ChannelType, DiscordAPIError, EmbedBuilder} from 'discord.js';
+import {ChannelType, DiscordAPIError, EmbedBuilder, Message} from 'discord.js';
 import {and, eq} from 'drizzle-orm';
 import {config, isProd} from '../config';
 import {db, persistentMessages} from '../db';
@@ -7,6 +7,10 @@ import {warStatusEmbeds} from './embed';
 import {logger} from './logging';
 
 const SUBSCRIBE_FOOTER = config.SUBSCRIBE_FOOTER;
+const cachedMessages: {
+  type: string;
+  message: Message<true>;
+}[] = [];
 
 export async function updateMessages() {
   // measure time taken to update all persistent messages
@@ -22,9 +26,77 @@ export async function updateMessages() {
       eq(persistentMessages.production, isProd)
     ),
   });
+  const uncachedMessages = messages.filter(
+    m => !cachedMessages.some(c => c.message.id === m.messageId)
+  );
+  logger.info(
+    `Updating ${cachedMessages.length} cached, ${uncachedMessages.length} uncached messages`,
+    {type: 'info'}
+  );
 
   const promises: Promise<any>[] = [];
-  for (const message of messages) {
+  for (const message of cachedMessages) {
+    try {
+      switch (message.type) {
+        case 'war_status':
+          promises.push(
+            message.message.edit({
+              embeds: embeds.curr_war,
+            })
+          );
+          break;
+      }
+    } catch (err) {
+      const discordErr = err as DiscordAPIError;
+      // discord API error codes
+      // https://github.com/meew0/discord-api-docs-1/blob/master/docs/topics/RESPONSE_CODES.md#json-error-response
+      logger.warn(`Error updating message: ${discordErr.message}`, {
+        type: 'update',
+        ...discordErr,
+      });
+      // switch (discordErr.code) {
+      //   case 10003: // Unknown channel
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, message.message.id),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 10008: // Unknown message
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, message.message.id),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 50001: // Missing access
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, message.message.id),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 50005: // Cannot edit a message authored by another user
+      //     break;
+      // }
+    }
+  }
+  for (const message of uncachedMessages) {
     const {messageId, channelId, type: messageType} = message;
 
     try {
@@ -39,7 +111,8 @@ export async function updateMessages() {
       ) {
         // try fetching the message, may throw '10008', message doesn't exist (deleted?)
         const discordMsg = await messageChannel.messages.fetch(messageId);
-        if (discordMsg)
+        if (discordMsg) {
+          cachedMessages.push({type: messageType, message: discordMsg});
           switch (messageType) {
             case 'war_status':
               promises.push(
@@ -49,51 +122,56 @@ export async function updateMessages() {
               );
               break;
           }
+        }
       }
     } catch (err) {
       const discordErr = err as DiscordAPIError;
       // discord API error codes
       // https://github.com/meew0/discord-api-docs-1/blob/master/docs/topics/RESPONSE_CODES.md#json-error-response
-      switch (discordErr.code) {
-        case 10003: // Unknown channel
-          promises.push(
-            db
-              .delete(persistentMessages)
-              .where(
-                and(
-                  eq(persistentMessages.messageId, messageId),
-                  eq(persistentMessages.production, isProd)
-                )
-              )
-          );
-          break;
-        case 10008: // Unknown message
-          promises.push(
-            db
-              .delete(persistentMessages)
-              .where(
-                and(
-                  eq(persistentMessages.messageId, messageId),
-                  eq(persistentMessages.production, isProd)
-                )
-              )
-          );
-          break;
-        case 50001: // Missing access
-          promises.push(
-            db
-              .delete(persistentMessages)
-              .where(
-                and(
-                  eq(persistentMessages.messageId, messageId),
-                  eq(persistentMessages.production, isProd)
-                )
-              )
-          );
-          break;
-        case 50005: // Cannot edit a message authored by another user
-          break;
-      }
+      logger.warn(`Error updating message: ${discordErr.message}`, {
+        type: 'update',
+        ...discordErr,
+      });
+      // switch (discordErr.code) {
+      //   case 10003: // Unknown channel
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, messageId),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 10008: // Unknown message
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, messageId),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 50001: // Missing access
+      //     promises.push(
+      //       db
+      //         .delete(persistentMessages)
+      //         .where(
+      //           and(
+      //             eq(persistentMessages.messageId, messageId),
+      //             eq(persistentMessages.production, isProd)
+      //           )
+      //         )
+      //     );
+      //     break;
+      //   case 50005: // Cannot edit a message authored by another user
+      //     break;
+      // }
     }
   }
 
